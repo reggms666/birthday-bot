266ad7c284f5875a8effa66191a0aab19b6b22af
reggms <rmuslyumova@mail.ru>
2025-05-15 14:00:54 +0300

    first commit

---
+++ database/birthday_db.py
@@ -0,0 +1,10 @@
+# database/birthday_db.py
+# Глобальный словарь для хранения дней рождения.
+# Формат: { user_id: { friend_name: "DD.MM", ... }, ... }
+birthdays: dict[int, dict[str, str]] = {}
+
+from services.storage_json import load_birthdays
+# database/birthday_db.py
+# Глобальный словарь для хранения дней рождения.
+# Формат: { user_id: { friend_name: "DD.MM", ... }, ... }
+birthdays: dict[int, dict[str, str]] = load_birthdays()
